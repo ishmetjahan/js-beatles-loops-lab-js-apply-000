942a850dee79151d17e8e7217f3e5a0a50312b8a
Ishmet <ishmetjahan@yahoo.com>
2019-05-30 14:07:23 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // add solution here
+function theBeatlesPlay (arrMus,arrInst){
+  let arr= []
+  for (let i=0; i<arrMus.length; i++){
+    for (let j=0; j<arrInst.length; j++){
+      let instr= arrInst[j]
+      arr.push(`${musican} plays ${instr}`)
+    }
+  }
+  return arr
+}
